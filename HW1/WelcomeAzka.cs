401f2037e2bc6ad5e76ba06318fd47d189b23a8b
Azka Noreen <azka.noreen6@gmail.com>
2022-10-19 21:37:37 +0500

    Home Task 1 Intro

---
+++ HW1/WelcomeAzka.cs
@@ -0,0 +1,16 @@
+namespace Greetings{
+	public class Welcome{
+		public static void Main(string [] args)
+		{
+			System.Console.WriteLine("Enter Name");
+			System.Console.WriteLine("Dilhum");
+						System.Console.WriteLine("Enter phone Number");
+			System.Console.WriteLine("03098787655");
+			System.Console.WriteLine("Enter Address");
+			System.Console.WriteLine("lhr");
+
+			System.Console.ReadKey();
+
+		}
+	}
+}
