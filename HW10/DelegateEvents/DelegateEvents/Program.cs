f0e3ca5d27025e822d0fd6d1f9b5636ca3d227a3
Azka Noreen <azka.noreen6@gmail.com>
2022-10-19 21:54:23 +0500

    Home Task 10 Delegates and Events

---
+++ HW10/DelegateEvents/DelegateEvents/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateEvents
+{
+    public delegate String MyDelagate(String mydelegateString);
+    class Program
+    {
+        event MyDelagate myevent;
+        public Program()
+        {
+            this.myevent += new MyDelagate(this.hi);
+        }
+        public String hi(String name)
+        {
+            return "Hi, " + name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Azka Noreen");
+            Console.WriteLine("BSEF19M502");
+            Program myProg = new Program();
+            String result = myProg.myevent("Azka Noreen");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW10/Events/Events/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Events
+{
+    public delegate void myDel();
+    class Program
+    {
+        public static event myDel Mevent;
+        public static void Family()
+        {
+            Console.WriteLine("Family");
+        }
+        public static void Friends()
+        {
+            Console.WriteLine("Friends");
+        }
+        public static void ClassFellows()
+        {
+            Console.WriteLine("Class Fellows");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Azka Noreen");
+            Console.WriteLine("BSEF19M502");
+            Mevent += new myDel(Family);
+            Mevent += new myDel(Friends);
+            Mevent += new myDel(ClassFellows);
+            Mevent.Invoke();
+            Console.ReadKey();
+        }
+    }}
+
+
